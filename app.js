b012fdeb6a518c1271619d5da73be01c4f9978da
Andrea Jemmett <andreajemmett@gmail.com>
2014-09-05 14:24:45 +0200

    Added a layer of security

--- app.js
+++ app.js
@@ -3,29 +3,51 @@ var url = require('url');
 var exec = require('child_process').exec;
 
 var config = {
-	port: 3737,
+	hostname: 'YOUR_SECRET_HOST',
+	port: 0,
+	secret: 'YOUR_YO_SECRET',
 	allowedUsers: ['ACIDGHOST']
 };
 
+try {
+	var configLocal = require('./config');
+	Object.keys(config).forEach(function(k) {
+		if(configLocal.hasOwnProperty(k)) {
+			config[k] = configLocal[k];
+		}
+	});
+	console.log('Configurations:', config);
+} catch(e) {}
+
 http.createServer(function(req, res) {
 
-	var user = url.parse(req.url, true).query.username;
-	console.log("YO from", user);
-	
-	for(var i=0; i<config.allowedUsers.length; i++) {
-		if (config.allowedUsers[i] === user) {
-			setTimeout(function() {
-				exec("killall chrome", function (error, stdout, stderr) {
-					console.log("Killing Chrome...");
-				});
-			}, 50000);
-			exec("shutdown -h +1", function (error, stdout, stderr) {
-				console.log("Shutdown...");
-			});
-      		break;
+	var urlParts = url.parse(req.url, true);
+	var user = urlParts.query.username;
+
+	if(user != undefined) {
+		console.log("YO from", user, req.connection.remoteAddress);
+
+		var checkHost = (req.headers.host === config.hostname+':'+config.port);
+		var checkPathname = (urlParts.pathname === '/'+config.secret);
+		if(checkHost && checkPathname) {
+			for(var i=0; i<config.allowedUsers.length; i++) {
+				if (config.allowedUsers[i] === user) {
+					setTimeout(function() {
+						exec("killall chrome", function (error, stdout, stderr) {
+							console.log("Killing Chrome...");
+						});
+					}, 50000);
+					exec("shutdown -h +1", function (error, stdout, stderr) {
+						console.log("Shutdown...");
+					});
+					break;
+				}
+			}
+		} else {
+			console.log('Unauthorized access!\n', req.headers, '\n', urlParts);
 		}
 	}
 
 }).listen(config.port, function() {
-	console.log('Listening on port ' + config.port + '...');
+	console.log('\nListening on http://' + config.hostname + ':' + config.port + '...');
 });
